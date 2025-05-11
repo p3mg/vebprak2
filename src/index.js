87ce9527ffc4a482eebdd24c2421ba689495f4eb
p3mg <p3mg@yandex.ru>
2025-05-11 14:51:18 +0300

    Kommit 3

---
+++ src/index.js
@@ -0,0 +1,9 @@
+export { Header } from "./Header";
+export { Footer } from "./footer";
+export { Shop } from "../layout/Shop";
+export { Preloader } from "../components/Preloader";
+
+// import { Header } from "./Header";
+// import { Footer } from "./footer";
+// import { Shop } from "./layout/Shop";
+// import { Preloader } from "./components/Preloader";
--- src/layout/index.js
+++
@@ -1,4 +0,0 @@
-export { Header } from "./Header";
-export { Footer } from "./footer";
-export { Shop } from "../layout/Shop";
-export { Preloader } from "../components/Preloader";
